23e32bdd9e9906baea652752402914d5118822ee
boscochanam <66779287+boscochanam@users.noreply.github.com>
2023-10-02 18:40:19 +0530

    idk

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,80 +1,58 @@
-import React,  { useEffect } from 'react';
-import hilltop from '../static/symbieat_images/hilltop.jfif';
-import lhbc from '../static/symbieat_images/lhbc.jpg';
-import sicsr from '../static/symbieat_images/sicsr.jpg';
-import smcw from '../static/symbieat_images/smcw.jpg';
-import viman_nagar from '../static/symbieat_images/viman_nagar.jpg';
+import React from 'react';
 
 function Carousel() {
-  // Define carousel items with location names and images
-  const carouselLocations = [
-    {
-      title: 'Lavale Hillbase Campus',
-      imageSrc: lhbc, 
-    },
-    {
-      title: 'SMCW Cafeteria',
-      imageSrc: smcw, 
-    },
-    {
-      title: 'Hilltop Campus',
-      imageSrc: hilltop, 
-    },
-    {
-      title: 'SYMBIEAT at SICSR & SiG.',
-      imageSrc: sicsr, 
-    },
-    {
-      title: 'Viman Nagar',
-      imageSrc: viman_nagar, 
-    },
-  ];
-
-  useEffect(() => {
-    // Programmatically activate the carousel and start auto-play
-    const myCarousel = new window.bootstrap.Carousel(document.getElementById('myCarousel'), {
-      interval: 3000, // Set the auto-play interval to 3000ms (3 seconds)
-    });
-    
-    // Ensure the carousel is paused when the component unmounts
-    return () => {
-      myCarousel.pause();
-    };
-  }, []);
-    
-
   return (
     <div id="myCarousel" className="carousel slide" data-bs-ride="carousel">
       {/* Carousel Indicators */}
       <ol className="carousel-indicators">
-        {carouselLocations.map((location, index) => (
-          <li
-            key={index}
-            data-bs-target="#myCarousel"
-            data-bs-slide-to={index}
-            className={index === 0 ? 'active' : ''}
-          ></li>
-        ))}
+        <li data-bs-target="#myCarousel" data-bs-slide-to="0" className="active"></li>
+        <li data-bs-target="#myCarousel" data-bs-slide-to="1"></li>
+        <li data-bs-target="#myCarousel" data-bs-slide-to="2"></li>
       </ol>
 
       {/* Carousel Inner */}
       <div className="carousel-inner">
-        {carouselLocations.map((location, index) => (
-          <div className={`carousel-item ${index === 0 ? 'active' : ''}`} key={index}>
-            <div className="d-flex justify-content-center align-items-center">
-              <img
-                src={location.imageSrc}
-                alt={location.title}
-                className="d-block w-100"
-                style={{ objectFit: 'cover', maxHeight: '400px' }}
-              />
-              <div className="carousel-caption d-none d-md-block">
-                <h1>{location.title}</h1>
-              </div>
-            </div>
-          </div>
-        ))}
+  <div className="carousel-item active">
+    <div className="d-flex justify-content-center align-items-center">
+      <img
+        src="https://www.collegebatch.com/static/clg-gallery/symbiosis-international-university-pune-242394.jpg"
+        alt="SIT"
+        className="d-block w-100"
+        style={{ objectFit: 'cover', maxHeight: '400px' }}
+      />
+      <div className="carousel-caption d-none d-md-block">
+        <h1>SIT</h1>
       </div>
+    </div>
+  </div>
+  <div className="carousel-item">
+    <div className="d-flex justify-content-center align-items-center">
+      <img
+        src="https://www.scit.edu/assets/images/campus/Cafeteria2.jfif"
+        alt="Campus 2"
+        className="d-block w-100"
+        style={{ objectFit: 'cover', maxHeight: '400px' }}
+      />
+      <div className="carousel-caption d-none d-md-block">
+        <h1>Campus 2</h1>
+      </div>
+    </div>
+  </div>
+  <div className="carousel-item">
+    <div className="d-flex justify-content-center align-items-center">
+      <img
+        src="https://fastly.4sqi.net/img/general/600x600/13144957_bcEv1_af9XJWcq6O_NJGJt-RLoRx0tnxWaw72iIYN_I.jpg"
+        alt="Campus"
+        className="d-block w-100"
+        style={{ objectFit: 'cover', maxHeight: '400px' }}
+      />
+      <div className="carousel-caption d-none d-md-block">
+        <h1>Campus 3</h1>
+      </div>
+    </div>
+  </div>
+</div>
+
 
       {/* Carousel Controls */}
       <a
