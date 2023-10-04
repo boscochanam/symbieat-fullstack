bd97a5c5a2c31d2301804479e6fba0cf9e428b05
boscochanam <66779287+boscochanam@users.noreply.github.com>
2023-10-05 00:12:32 +0530

    Updated link for symbieat logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,7 +19,7 @@ function Header(props) {
   return (
     <nav className="navbar bg-dark navbar-expand-lg" style={{ backgroundColor: '#EAEAEA' }} data-bs-theme="dark">
       <div className="container-fluid">
-        <a className="navbar-brand" href="https://www.google.com/">Symbieat</a>
+        <NavLink className="navbar-brand" to="/">Symbieat</NavLink>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
