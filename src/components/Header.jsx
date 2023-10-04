525b5a792f223b4662e2ec7fe6eece1f4c58c080
boscochanam <66779287+boscochanam@users.noreply.github.com>
2023-10-05 01:58:29 +0530

    Removed extra search

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -94,16 +94,7 @@ function Header(props) {
           <div className="input-group">
             <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" /> &nbsp;
             <button className="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
-            <input
-              className="form-control mr-sm-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
             &nbsp;
-            <button className="btn btn-outline-light my-2 my-sm-0" type="submit">
-              Search
-            </button>
           </div>
         </form>
       </div>
