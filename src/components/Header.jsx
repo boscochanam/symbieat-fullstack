1400d62222b49c0049a2b400e4dff85e21e393f2
Ashay-S <112310266+Ashay-S@users.noreply.github.com>
2023-10-05 00:23:05 +0530

    fixed burger menu, added logo and some text edits

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { NavLink } from 'react-router-dom';
 
 function Header(props) {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   const cartCountStyle = {
     weight: 'strong',
     padding: '5px',
@@ -11,49 +13,95 @@ function Header(props) {
   };
 
   const resetCart = () => {
-    props.setCartCount(0); // Assuming you have a state setter for cartCount
+    props.setCartCount(0);
     props.setCartTotal(0);
     props.clearCart();
+  };
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
   };
 
   return (
     <nav className="navbar bg-dark navbar-expand-lg" style={{ backgroundColor: '#EAEAEA' }} data-bs-theme="dark">
       <div className="container-fluid">
-        <a className="navbar-brand" href="https://www.google.com/">Symbieat</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <a className="navbar-brand" href="https://www.google.com/">
+          <img src="../favicon.ico" alt="Symbieat Logo" height="30" />
+        </a>
+        <button
+          className={`navbar-toggler ${menuOpen ? '' : 'collapsed'}`}
+          type="button"
+          aria-label="Toggle navigation"
+          onClick={toggleMenu}
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
+        <div className={`collapse navbar-collapse ${menuOpen ? 'show' : ''}`} id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <NavLink className="nav-link" aria-current="page" to="/">Home</NavLink>
+              <NavLink className="nav-link" aria-current="page" to="/" onClick={closeMenu}>
+                Home
+              </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/menu">Menu</NavLink>
+              <NavLink className="nav-link" to="/menu" onClick={closeMenu}>
+                Menu
+              </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/locations">Locations</NavLink>
+              <NavLink className="nav-link" to="/locations" onClick={closeMenu}>
+                Locations
+              </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/cart">Cart</NavLink>
+              <NavLink className="nav-link" to="/cart" onClick={closeMenu}>
+                Cart
+              </NavLink>
             </li>
           </ul>
         </div>
-        <button className='btn btn-success' onClick={() => { resetCart(); window.location.href = "/login"; }}>Log In</button>
+        <button
+          className='btn btn-success'
+          onClick={() => {
+            resetCart();
+            window.location.href = "/login";
+          }}
+        >
+          Log In
+        </button>
         <div style={cartCountStyle}>
           <ShoppingCartIcon fontSize="small" />
           &nbsp; {props.cartCount} : &nbsp; &#8377; {props.cartTotal} &nbsp; &nbsp;
-          <button className="btn btn-outline-light my-2 my-sm-0" onClick={() => { resetCart(); }}>Clear Cart</button>
+          <button
+            className="btn btn-outline-light my-2 my-sm-0"
+            onClick={() => {
+              resetCart();
+            }}
+          >
+            Clear Cart
+          </button>
         </div>
-        <form className="form-inline ml-auto" role="search"> {/* 'ml-auto' pushes the form to the right */}
-          <div className="input-group"> {/* Use Bootstrap's input group for side-by-side elements */}
-            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" /> &nbsp;
-            <button className="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+        <form className="form-inline ml-auto" role="search">
+          <div className="input-group">
+            <input
+              className="form-control mr-sm-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            &nbsp;
+            <button className="btn btn-outline-light my-2 my-sm-0" type="submit">
+              Search
+            </button>
           </div>
         </form>
       </div>
     </nav>
-  )
+  );
 }
 
 export default Header;
