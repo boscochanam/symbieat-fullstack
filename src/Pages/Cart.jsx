07f9bfddd3e632deb11ba0047c9592f57a8b4429
AnirudhRautela7 <rautelaanirudh7703@gmail.com>
2023-10-23 03:01:11 +0530

    improved cart

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,13 +1,9 @@
 import React, { useState } from 'react';
 import Header from '../components/Header';
-// import Footer from '../components/Footer';
-import '../static/style.css';
 import Popup from '../components/Popup';
 import { NavLink } from 'react-router-dom';
 
-
 function Cart({ cartItems, cartTotal, setCartTotal, ...props }) {
-
   const [showPopup, setShowPopup] = useState(false);
 
   const handlePlaceOrder = () => {
@@ -17,7 +13,17 @@ function Cart({ cartItems, cartTotal, setCartTotal, ...props }) {
     // Clear the cart (You should implement this function)
   };
 
-  
+  // Create a modified cart data structure to group items by title
+  const groupedCartItems = cartItems.reduce((accumulator, currentItem) => {
+    const { title, price } = currentItem;
+    if (!accumulator[title]) {
+      accumulator[title] = { ...currentItem, quantity: 0 };
+    }
+    accumulator[title].quantity += 1;
+    accumulator[title].totalPrice = accumulator[title].quantity * price;
+    return accumulator;
+  }, {});
+
   return (
     <div className="wrapper">
       {/* Header */}
@@ -29,7 +35,7 @@ function Cart({ cartItems, cartTotal, setCartTotal, ...props }) {
         clearCart={props.clearCart}
         loginState={props.loginState}
         setLoginState={props.loginState}
-        login={props.login} 
+        login={props.login}
       />
 
       {/* Main Content */}
@@ -39,29 +45,30 @@ function Cart({ cartItems, cartTotal, setCartTotal, ...props }) {
             {/* Left Column - Cart Items (Half of the page) */}
             <div className="col-md-6">
               <h2 className="text-center mb-4">Your Shopping Cart</h2>
-              {cartItems.length === 0 ? (
-                <p className="text-center">Your cart is empty.</p>
-              ) : (
-                <div>
-                  {cartItems.map((item, index) => (
-                    <div className="mb-4" key={index}>
-                      <div className="card">
-                        <img
-                          src={item.imageSrc}
-                          className="card-img-top"
-                          alt={item.title}
-                          style={{ height: '50px', objectFit: 'cover' }}
-                        />
-                        <div className="card-body text-center">
-                          <h5 className="card-title">{item.title}</h5>
-                          <p className="card-text">{item.description}</p>
-                          <p className="card-text">&#8377; {item.price}</p>
-                        </div>
-                      </div>
+              {Object.values(groupedCartItems).map((cartItem, index) => (
+                <div className="mb-4" key={index}>
+                  <div className="card">
+                    <img
+                      src={cartItem.imageSrc}
+                      className="card-img-top"
+                      alt={cartItem.title}
+                      style={{ height: '50px', objectFit: 'cover' }}
+                    />
+                    <div className="card-body text-center">
+                      <h5 className="card-title">
+                        {cartItem.title} (Quantity: {cartItem.quantity})
+                      </h5>
+                      <p className="card-text">{cartItem.description}</p>
+                      <p className="card-text">
+                        Price per item: &#8377; {cartItem.price.toFixed(2)}
+                      </p>
+                      <p className="card-text">
+                        Total price: &#8377; {cartItem.totalPrice.toFixed(2)}
+                      </p>
                     </div>
-                  ))}
+                  </div>
                 </div>
-              )}
+              ))}
             </div>
 
             {/* Right Column - Order Summary (Half of the page) */}
@@ -69,15 +76,22 @@ function Cart({ cartItems, cartTotal, setCartTotal, ...props }) {
               <div className="order-summary">
                 <h2 className="text-center mb-4">Order Summary</h2>
                 <div className="order-summary-items">
-                  {cartItems.map((item, index) => (
-                    <p key={index} className="order-item" style={{}}>
+                  {Object.values(groupedCartItems).map((cartItem, index) => (
+                    <p key={index} className="order-item">
                       <span className="item-number">Item {index + 1}: </span>
-                      {item.title} &#8377; {item.price}
+                      {cartItem.title} (Quantity: {cartItem.quantity}) &#8377;{' '}
+                      {cartItem.totalPrice.toFixed(2)}
                     </p>
                   ))}
                 </div>
                 <p className="total">Total: &#8377; {cartTotal.toFixed(2)}</p>
-                <NavLink to="/payment" className="btn btn-primary btn-block place-order-button">Place Order</NavLink>
+                <NavLink
+                  to="/payment"
+                  className="btn btn-primary btn-block place-order-button"
+                  onClick={handlePlaceOrder}
+                >
+                  Place Order
+                </NavLink>
               </div>
             </div>
           </div>
